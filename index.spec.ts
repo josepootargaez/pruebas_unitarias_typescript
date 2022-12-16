fa5c78588a315439de3354d0db7f4e5e05448c59
jose-poot <jose-9803@hotmail.com>
2022-12-15 19:54:13 -0500

    se agrega crud para pruebas unitarias

--- index.spec.ts
+++ index.spec.ts
@@ -7,3 +7,35 @@ describe("GET /ping", () => {
     expect(response.statusCode).toBe(200)
   });
 });
+
+
+describe("post /createUser", () => {
+  test("test response status code with 201 ", async () => {
+    const payload ={
+      name:"eduardo",
+      email:"eduardo@hotmail.com"
+  }
+ const response = await request(app).post("/createUser").send(payload);
+    expect(response.statusCode).toBe(201)
+  });
+});
+
+describe("delete /deleteUser by id", () => {
+  test("test response status code with 204 ", async () => {
+    const id = 1;
+    const response = await request(app).delete(`/deleteUser/${id}`).send();
+    expect(response.statusCode).toBe(204)
+  });
+});
+
+describe("patch /updateUser by id", () => {
+  test("test response status code with 204 ", async () => {
+    const payload ={
+      name:"eduardo",
+      email:"eduardo@hotmail.com"
+  }
+    const id = 1;
+    const response = await request(app).patch(`/updateUser/${id}`).send(payload);
+    expect(response.statusCode).toBe(204)
+  });
+});
--- index.ts
+++ index.ts
@@ -2,7 +2,18 @@ import express  from "express";
 const app= express();
 
 app.get("/ping",(req,res)=>{
-    res.send("hola");
+    res.status(200).json({status:true})
+});
+
+app.post("/createUser",(req,res)=>{
+    res.status(201).json({status:true})
+})
+
+app.patch("/updateUser/:id",(req,res)=>{
+    res.status(204).json({status:true})
+})
+app.delete("/deleteUser/:id",(req,res)=>{
+    res.status(204).json({status:true})
 })
  app.listen(2000);
 console.log("Server is running on port 2000");
